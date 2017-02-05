e5c232db308d13288681664e1625d92c17c2638a
Chandni Patel <chandnir.patel@gmail.com>
2017-02-05 09:33:50 -0800

    Initial commit
    Includes README.md, gitignore, simple test, and hello world.

---
+++ src/test/java/com/chandni/tdd/MainTest.java
@@ -0,0 +1,12 @@
+package com.chandni.tdd;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void sanityCheck() throws Exception {
+        assertTrue(true);
+
+    }
+}
